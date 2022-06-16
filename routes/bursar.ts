e3031f744a15f66305271474b587d07a19d6fbcf
Jubril Musa <41811898+Muhsar@users.noreply.github.com>
2022-06-16 02:57:16 +0100

    na only dashboard remain bayiiiii

--- routes/bursar.ts
+++ routes/bursar.ts
@@ -5,7 +5,8 @@ import BursarController from '../controller/BursarController';
 router.use(cors())
 
 router.post("/login", (req, res)=> BursarController.Login(req, res))
-router.post("/not_verified", (req, res)=> BursarController.SetNotVerified(req, res))
-router.post("/verified", (req, res)=> BursarController.SetVerified(req, res))
+router.get("/bookings", (req, res)=> BursarController.GetAllSentBookings(req, res))
+router.post("/not_verified:booking_id", (req, res)=> BursarController.SetNotVerified(req, res))
+router.post("/verified:booking_id", (req, res)=> BursarController.SetVerified(req, res))
 
 module.exports = router
